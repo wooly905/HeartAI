2929eb45870edee36a2ac38d70f865e0b62b50b2
wooly905 <bruceyclee@gmail.com>
2019-11-16 21:28:20 +0800

    fitler small unwanted contours

--- heart.py
+++ heart.py
@@ -6,34 +6,14 @@ import os
 
 print("cv2.__version__ : ", cv2.__version__)
 
-filename = "01499407_094944_0272.DCM"  # get_testdata_files('MR_small.dcm')[0]
-ds = pydicom.dcmread(filename)
+# define constants
+RedColor = (0, 0, 255)
+dcmFolder = "c:\\temp2\\"
+dcmFilePath =  os.path.join(dcmFolder, "01499407_094944_0272.DCM")  # get_testdata_files('MR_small.dcm')[0]
+contourAreaThreshold = 2000
 
-# def imageStruct(ds):
-#     # get the pixel information into a numpy array
-#     data = ds.pixel_array
-#     print('The dataset has {} {}x{} images'.format(data.shape[0], data.shape[1], data.shape[2]))
-#     #for i in range(0, data.shape[0]):
-#     #    plt.imshow(data[i])
-#     plt.subplot(2, 2, 1)
-#     # plt.imshow(data[0, :, :, 0])
-#     # plt.title('channel 0 (R)')
-#     img = cv2.merge( (data[0, :, :, 0], data[0, :, :, 1], data[0, :, :, 2]) )
-#     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     plt.imshow(gray_img)
-#     plt.title('test')
-#     # plt.subplot(2, 2, 2)
-#     # plt.imshow(data[0, :, :, 1])
-#     # plt.title('channel 1 (G)')
-#     # plt.subplot(2, 2, 3)
-#     # plt.imshow(data[0, :, :, 2])
-#     # plt.title('channel 2 (B)')
-#     plt.subplot(2, 2, 4)
-#     plt.imshow(data[0])
-#     plt.title('channel all')
-#     plt.show()
-
-#imageStruct(ds)
+if (os.path.exists(dcmFilePath)):
+    originalImageDataSet = pydicom.dcmread(dcmFilePath)
 
 def displayImage(image1, image2):
     plt.subplot(1, 2, 1)
@@ -44,52 +24,31 @@ def displayImage(image1, image2):
 
 def createCurrentDirectory(newDirName):
     currentDir = os.getcwd()
-    os.mkdir(currentDir + '/' + newDirName)
+    newPath = os.path.join(currentDir, newDirName)
+    os.mkdir(newPath)
     
-print('Patient id : ' + ds.PatientID)
-# create a new folder
-#createCurrentDirectory(ds.PatientID)
-
+def filterContours(contours):
+    result = list()
+    for contour in contours:
+        if (cv2.contourArea(contour) > contourAreaThreshold):
+            result.append(contour)
+    return result
+    
 # pixel_array needs Pillow package
-data = ds.pixel_array
-imageCount = data.shape[0]
+imagePixels = originalImageDataSet.pixel_array
+imageCount = imagePixels.shape[0]
 index = 0
-
-# while index < 1: #imageCount:
-#     # merge BGR
-#     img = cv2.merge((data[index, :, :, 0], data[index, :, :, 1], data[index, :, :, 2]))
-#     contourImage = img.copy()
-#     # convert to gray
-#     grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     # equalizeHist
-#     grayImageEqu = cv2.equalizeHist(grayImage)
-#     # filtering
-#     copyGrayImage = grayImage.copy()
-#     copyGrayImage = cv2.medianBlur(copyGrayImage, 7)
-#     copyGrayImage[grayImageEqu < 150] = 0
-#     copyGrayImage[grayImageEqu >= 200] = 255
-#     grayImageEqu2 = cv2.equalizeHist(copyGrayImage)
-#     copyGrayImage[grayImageEqu2 < 150] = 0
-#     copyGrayImage[grayImageEqu2 >= 200] = 255
-#     medianGrayImage = cv2.medianBlur(copyGrayImage, 7)
-#     #displayImage(img, medianGrayImage)
-#     contours, hier = cv2.findContours(medianGrayImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   #cv2.RETR_EXTERNAL
-#     # draw contours
-#     cv2.drawContours(medianGrayImage, contours, -1, (0, 0, 255), 2)
-#     #displayImage(img, medianGrayImage)
-#     cv2.imwrite(str(index) + ".png", medianGrayImage)
-#     index = index + 1
 
 while index < 1: #imageCount:
     # merge BGR
-    img = cv2.merge((data[index, :, :, 0], data[index, :, :, 1], data[index, :, :, 2]))
-    displayImage(img, img)
+    img = cv2.merge((imagePixels[index, :, :, 0], imagePixels[index, :, :, 1], imagePixels[index, :, :, 2]))
+    #displayImage(img, img)
     contourImage = img.copy()
     invertedImg = (255 - img)
-    displayImage(invertedImg, invertedImg)
+    #displayImage(invertedImg, invertedImg)
     # convert to gray
     grayImage = cv2.cvtColor(invertedImg, cv2.COLOR_BGR2GRAY)
-    displayImage(grayImage, grayImage)
+    #displayImage(grayImage, grayImage)
     # equalizeHist
     grayImageEqu = cv2.equalizeHist(grayImage)
     # filtering
@@ -97,18 +56,22 @@ while index < 1: #imageCount:
     copyGrayImage = cv2.medianBlur(copyGrayImage, 7)
     copyGrayImage[grayImageEqu < 50] = 0
     copyGrayImage[grayImageEqu >= 150] = 255
-    displayImage(grayImageEqu, copyGrayImage)
+    #displayImage(grayImageEqu, copyGrayImage)
     # equalize histogram 
     grayImageEqu2 = cv2.equalizeHist(copyGrayImage)
     # filtering (blur and ...)
     grayImageEqu2 = cv2.medianBlur(grayImageEqu2, 7)
     copyGrayImage[grayImageEqu2 < 20] = 0
     copyGrayImage[grayImageEqu2 >= 250] = 255
-    displayImage(grayImageEqu2, copyGrayImage)
+    #displayImage(grayImageEqu2, copyGrayImage)
     contours, hier = cv2.findContours(copyGrayImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   #cv2.RETR_EXTERNAL
+    print("length of all contours = " + str(len(contours)))
+    qualifiedContours = filterContours(contours)
+    print("length of qualified contours = " + str(len(qualifiedContours)))
     # draw contours
-    cv2.drawContours(contourImage, contours, -1, (0, 0, 255), 2)
+    cv2.drawContours(contourImage, qualifiedContours, -1, RedColor, 2)
     #displayImage(img, medianGrayImage)
     #combinedImage = np.concatenate((img, contourImage), axis=1)
+    displayImage(img, contourImage)
     cv2.imwrite(str(index) + ".png", contourImage)
     index = index + 1
