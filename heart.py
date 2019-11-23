3e32c917c096242c5953cb4377e556e6e76c0f16
wooly905 <bruceyclee@gmail.com>
2019-11-24 00:07:11 +0800

    code update

--- heart.py
+++ heart.py
@@ -9,69 +9,81 @@ print("cv2.__version__ : ", cv2.__version__)
 # define constants
 RedColor = (0, 0, 255)
 dcmFolder = "c:\\temp2\\"
-dcmFilePath =  os.path.join(dcmFolder, "01499407_094944_0272.DCM")  # get_testdata_files('MR_small.dcm')[0]
+dcmFilePath =  os.path.join(dcmFolder, "01605286_112110_0592.DCM")  # get_testdata_files('MR_small.dcm')[0]
 contourAreaThreshold = 2000
 
 if (os.path.exists(dcmFilePath)):
     originalImageDataSet = pydicom.dcmread(dcmFilePath)
 
-def displayImage(image1, image2):
+def DisplayImage(image1, image2):
     plt.subplot(1, 2, 1)
     plt.imshow(image1)
     plt.subplot(1, 2, 2)
     plt.imshow(image2)
     plt.show()
 
-def createCurrentDirectory(newDirName):
+def CreateCurrentDirectory(newDirName):
     currentDir = os.getcwd()
     newPath = os.path.join(currentDir, newDirName)
     os.mkdir(newPath)
     
-def filterContours(contours):
+def FilterContours(contours):
     result = list()
     for contour in contours:
         if (cv2.contourArea(contour) > contourAreaThreshold):
             result.append(contour)
     return result
-    
-# pixel_array needs Pillow package
-imagePixels = originalImageDataSet.pixel_array
-imageCount = imagePixels.shape[0]
-index = 0
 
-while index < 1: #imageCount:
-    # merge BGR
-    img = cv2.merge((imagePixels[index, :, :, 0], imagePixels[index, :, :, 1], imagePixels[index, :, :, 2]))
-    #displayImage(img, img)
-    contourImage = img.copy()
-    invertedImg = (255 - img)
-    #displayImage(invertedImg, invertedImg)
-    # convert to gray
-    grayImage = cv2.cvtColor(invertedImg, cv2.COLOR_BGR2GRAY)
-    #displayImage(grayImage, grayImage)
-    # equalizeHist
+def InvertImage(image):
+    return (255-img)
+
+def Filtering(image):
     grayImageEqu = cv2.equalizeHist(grayImage)
-    # filtering
     copyGrayImage = grayImage.copy()
     copyGrayImage = cv2.medianBlur(copyGrayImage, 7)
     copyGrayImage[grayImageEqu < 50] = 0
     copyGrayImage[grayImageEqu >= 150] = 255
-    #displayImage(grayImageEqu, copyGrayImage)
     # equalize histogram 
     grayImageEqu2 = cv2.equalizeHist(copyGrayImage)
     # filtering (blur and ...)
     grayImageEqu2 = cv2.medianBlur(grayImageEqu2, 7)
     copyGrayImage[grayImageEqu2 < 20] = 0
     copyGrayImage[grayImageEqu2 >= 250] = 255
+    return copyGrayImage
+
+def CropImage(image):
+    x=0
+    y=60
+    h=700
+    w=800
+    return image[y: y + h, x: x + w]
+
+# pixel_array needs Pillow package
+imagePixels = originalImageDataSet.pixel_array
+imageCount = imagePixels.shape[0]
+index = 0
+
+while index < imageCount:
+    # merge BGR
+    img = cv2.merge((imagePixels[index, :, :, 0], imagePixels[index, :, :, 1], imagePixels[index, :, :, 2]))
+    img = CropImage(img)
+    #displayImage(img, img)
+    contourImage = img.copy()
+    invertedImg = InvertImage(img)
+    #displayImage(invertedImg, invertedImg)
+    # convert to gray
+    grayImage = cv2.cvtColor(invertedImg, cv2.COLOR_BGR2GRAY)
+    #displayImage(grayImage, grayImage)
+    filteringImage = Filtering(grayImage)
     #displayImage(grayImageEqu2, copyGrayImage)
-    contours, hier = cv2.findContours(copyGrayImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   #cv2.RETR_EXTERNAL
-    print("length of all contours = " + str(len(contours)))
-    qualifiedContours = filterContours(contours)
-    print("length of qualified contours = " + str(len(qualifiedContours)))
+    contours, hier = cv2.findContours(filteringImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   #cv2.RETR_EXTERNAL
+    print("Number of all contours = " + str(len(contours)))
+    qualifiedContours = FilterContours(contours)
+    print("Number of qualified contours = " + str(len(qualifiedContours)))
     # draw contours
     cv2.drawContours(contourImage, qualifiedContours, -1, RedColor, 2)
     #displayImage(img, medianGrayImage)
-    #combinedImage = np.concatenate((img, contourImage), axis=1)
-    displayImage(img, contourImage)
-    cv2.imwrite(str(index) + ".png", contourImage)
+    combinedImage = np.concatenate((img, contourImage), axis=1)
+    #DisplayImage(img, contourImage)
+    cv2.imwrite(os.path.join(dcmFolder, str(index) + ".png"), combinedImage)
     index = index + 1
