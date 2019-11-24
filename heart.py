55c8311251f35aa058048c541eab747c27530a74
wooly905 <bruceyclee@gmail.com>
2019-11-24 20:48:14 +0800

    update arc view and muscle view

--- heart.py
+++ heart.py
@@ -8,6 +8,7 @@ print("cv2.__version__ : ", cv2.__version__)
 
 # define constants
 RedColor = (0, 0, 255)
+BlueColor = (255, 0, 0)
 dcmFolder = "c:\\temp2\\"
 dcmFilePath =  os.path.join(dcmFolder, "01605286_112110_0592.DCM")  # get_testdata_files('MR_small.dcm')[0]
 contourAreaThreshold = 2000
@@ -37,33 +38,29 @@ def FilterContours(contours):
 def InvertImage(image):
     return (255-img)
 
-def FilteringArcView(image):
+def ConvertToGray(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+def FilteringInternal(grayImage, blurSize, firstLowerValue, firstHighValue, secondLowerValue, secondHighValue):
     grayImageEqu = cv2.equalizeHist(grayImage)
     copyGrayImage = grayImage.copy()
-    copyGrayImage = cv2.medianBlur(copyGrayImage, 7)
-    copyGrayImage[grayImageEqu < 150] = 0
-    copyGrayImage[grayImageEqu >= 200] = 255
-    # equalize histogram 
+    copyGrayImage = cv2.medianBlur(copyGrayImage, blurSize)
+    copyGrayImage[grayImageEqu < firstLowerValue] = 0
+    copyGrayImage[grayImageEqu >= firstHighValue] = 255
     grayImageEqu2 = cv2.equalizeHist(copyGrayImage)
-    # filtering (blur and ...)
-    grayImageEqu2 = cv2.medianBlur(grayImageEqu2, 7)
-    copyGrayImage[grayImageEqu2 < 150] = 0
-    copyGrayImage[grayImageEqu2 >= 200] = 255
-    return copyGrayImage
+    grayImageEqu2 = cv2.medianBlur(grayImageEqu2, blurSize)
+    copyGrayImage[grayImageEqu2 < secondLowerValue] = 0
+    copyGrayImage[grayImageEqu2 >= secondHighValue] = 255
+    return copyGrayImage 
+
+def FilteringMuscleView(image):
+    grayImage = ConvertToGray(image)
+    return FilteringInternal(grayImage, 7, 150, 200, 150, 200)
    
 def FilteringChamberView(image):
-    grayImageEqu = cv2.equalizeHist(grayImage)
-    copyGrayImage = grayImage.copy()
-    copyGrayImage = cv2.medianBlur(copyGrayImage, 7)
-    copyGrayImage[grayImageEqu < 50] = 0
-    copyGrayImage[grayImageEqu >= 150] = 255
-    # equalize histogram 
-    grayImageEqu2 = cv2.equalizeHist(copyGrayImage)
-    # filtering (blur and ...)
-    grayImageEqu2 = cv2.medianBlur(grayImageEqu2, 7)
-    copyGrayImage[grayImageEqu2 < 20] = 0
-    copyGrayImage[grayImageEqu2 >= 250] = 255
-    return copyGrayImage
+    image = InvertImage(image)
+    grayImage = ConvertToGray(image)
+    return FilteringInternal(grayImage, 7, 50, 150, 20, 250)
 
 def CropImage(image):
     x=0
@@ -81,23 +78,17 @@ while index < imageCount:
     # merge BGR
     img = cv2.merge((imagePixels[index, :, :, 0], imagePixels[index, :, :, 1], imagePixels[index, :, :, 2]))
     img = CropImage(img)
-    #displayImage(img, img)
     contourImage = img.copy()
-    invertedImg = InvertImage(img)
-    #displayImage(invertedImg, invertedImg)
-    # convert to gray
-    grayImage = cv2.cvtColor(invertedImg, cv2.COLOR_BGR2GRAY)
-    #displayImage(grayImage, grayImage)
-    filteringImage = FilteringChamberView(grayImage)
-    #displayImage(grayImageEqu2, copyGrayImage)
-    contours, hier = cv2.findContours(filteringImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)   #cv2.RETR_EXTERNAL
-    print("Number of all contours = " + str(len(contours)))
-    qualifiedContours = FilterContours(contours)
-    print("Number of qualified contours = " + str(len(qualifiedContours)))
+    filteringChamberImage = FilteringChamberView(img)
+    contoursChamber, hierChanmber = cv2.findContours(filteringChamberImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  
+    filteringMuscleImage = FilteringMuscleView(img)
+    contourMuscle, hierMuscle = cv2.findContours(filteringMuscleImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #cv2.RETR_EXTERNAL
+    qualifiedChamberContours = FilterContours(contoursChamber)
+    qualifiedMuscleContours = FilterContours(contourMuscle)
     # draw contours
-    cv2.drawContours(contourImage, qualifiedContours, -1, RedColor, 2)
-    #displayImage(img, medianGrayImage)
+    cv2.drawContours(contourImage, qualifiedChamberContours, -1, RedColor, 2)
+    cv2.drawContours(contourImage, qualifiedMuscleContours, -1, BlueColor, 2)
     combinedImage = np.concatenate((img, contourImage), axis=1)
-    #DisplayImage(img, contourImage)
     cv2.imwrite(os.path.join(dcmFolder, str(index) + ".png"), combinedImage)
+    print(str(index) + ".png is created.")
     index = index + 1
